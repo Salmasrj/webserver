5bc599a3f931e517d9d9376c99b449eab99b6e15
Salmasrj <89201396+Salmasrj@users.noreply.github.com>
2024-01-16 11:21:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const database = require('./database');
-const users = require('./database');
+const database = require('./database.js');
+
+
 
 //static pour avoir accès au data et urlencoded pour recuperer les post
 app.use(express.static('public'))
@@ -17,26 +18,28 @@ app.post('/api/login',function(req,res){
   let hasAuthenticatedUser = false;
 
   for (let i = 0; i < database.users.length; i++){
-    const userToCheck = users[i];
+    const userToCheck = database.users[i];
     if (userToCheck.username == req.body.username && userToCheck.password == req.body.password){
-      res.send("Authenticated !");
       hasAuthenticatedUser = true;
+      const token = userToCheck.username + '_' + Date.now();
+      res.send("Authenticated ! You are : " + token);
       break;
     }
   }
 
   if (hasAuthenticatedUser === false){
-    res.send("Unauthenticated !");
+    res.redirect(401, '/');
+    console.log("Unauthenticated !");
   }
   
   console.log(req.body); //user.body.username affiche juste username
 
-  if (req.body.uname === 'salmaS' && req.body.psw === 'sriji' ){
+  /*if (req.body.uname === 'salmaS' && req.body.psw === 'sriji' ){
       res.send('You are logged in ! Password right')
   }
   else {
       res.send('Username or password wrong')
-  }
+  }*/
 })
 
 app.listen(port, function() {
