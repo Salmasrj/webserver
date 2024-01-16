b1f7d923be5018b6aac1a7e713d9a02ec0fee42a
Salmasrj <89201396+Salmasrj@users.noreply.github.com>
2024-01-16 10:19:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function (req, res) {
+const database = require('./database');
+const users = require('./database');
+
+//static pour avoir accès au data et urlencoded pour recuperer les post
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/test', function (req, res) {
   res.send('Hello World!')
+})
+
+app.post('/api/login',function(req,res){
+  let hasAuthenticatedUser = false;
+
+  for (let i = 0; i < database.users.length; i++){
+    const userToCheck = users[i];
+    if (userToCheck.username == req.body.username && userToCheck.password == req.body.password){
+      res.send("Authenticated !");
+      hasAuthenticatedUser = true;
+      break;
+    }
+  }
+
+  if (hasAuthenticatedUser === false){
+    res.send("Unauthenticated !");
+  }
+  
+  console.log(req.body); //user.body.username affiche juste username
+
+  if (req.body.uname === 'salmaS' && req.body.psw === 'sriji' ){
+      res.send('You are logged in ! Password right')
+  }
+  else {
+      res.send('Username or password wrong')
+  }
 })
 
 app.listen(port, function() {
